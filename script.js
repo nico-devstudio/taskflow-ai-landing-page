8f5f5cf50d12f3ddca28e82c4dbb959fe9b32164
Nico Angelo  Garilva <nicogarilva@gmail.com>
2026-07-02 03:54:41 +0800

    added js code

---
+++ script.js
@@ -0,0 +1,19 @@
+const navMenu = document.getElementById('nav-menu');
+const menuButton = document.querySelector('.menu');
+const closeButton = document.querySelector('.close');
+const navLink = document.querySelectorAll('.nav__link');
+
+menuButton.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+})
+
+const removeMenu = () => {
+    navMenu.classList.remove('show-menu');
+}
+closeButton.addEventListener('click', removeMenu)
+
+
+
+navLink.forEach(link => {
+    link.addEventListener('click', removeMenu)
+})
